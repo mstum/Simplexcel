58f9ef8f2313e2e8690ce538dbffd247258695a7
Michael Stum <git@stum.de>
2013-01-10 01:14:59 -0800

    Updated AssemblyInfo

--- src/Simplexcel/Properties/AssemblyInfo.cs
+++ src/Simplexcel/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Simplexcel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simplexcel .xlsx library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("http://www.stum.de")]
 [assembly: AssemblyProduct("Simplexcel")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2013 Michael Stum")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
