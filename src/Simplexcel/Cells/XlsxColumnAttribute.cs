de92d818a852266d28d090bb74c0adb7f6ef81f0
Michael Stum <git@stum.de>
2017-09-24 20:57:31 -0400

    Create initial attributes for attribute-driven Worksheet.FromData

---
+++ src/Simplexcel/Cells/XlsxColumnAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Simplexcel.Cells
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    internal class XlsxColumnAttribute : Attribute
+    {
+        /// <summary>
+        /// The name of the Column, used as the Header row
+        /// </summary>
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    /// This attribute causes <see cref="Worksheet.FromData"/> to ignore the property completely
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    internal class XlsxIgnoreColumnAttribute : Attribute
+    {
+    }
+}
