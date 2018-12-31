6f82ec6ba3a02a7cf23d7a68a85ad750d0cd16dd
Michael Stum <git@stum.de>
2018-12-31 14:25:30 -0500

    Update build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -25,34 +25,33 @@ try {
     $productionBranch = Build-GetPropertyOrDefault $buildProps "build_prod_branch" "master"
     $buildConfig = Build-GetPropertyOrDefault $buildProps "build_config" "Release"
 
+    Build-WriteTitle "Getting git branch information"
+    $commitCount = & git rev-list --count HEAD
+    $branchName = Build-GetPropertyOrDefault $buildProps "GIT_BRANCH" "$(& git rev-parse --abbrev-ref HEAD)"
+    $isProdBuild = $branchName -eq $productionBranch
+    Build-Log-Information "Git branch: $branchName, commit count: $commitCount, is production branch: $isProdBuild"
 
     Build-WriteTitle "Determining Version Number"
-    $commitCount = & git rev-list --count HEAD
-    $branchName = & git rev-parse --abbrev-ref HEAD
     $part3 = [math]::Floor($commitCount / 65535)
-    $part4 = $commitCount % 65535
-
-    $isProdBuild = $branchName -eq $productionBranch
+    $part4 = $commitCount % 65535    
     $versionSuffix = $(If ($isProdBuild) { "" } Else {  "-$branchName" })
-
     $version = "$version.$part3.$part4"
+    Build-Log-Information "Version: $version$versionSuffix"
 
-    Write-Host "Version: $version$versionSuffix"
-
-    Write-Host "Remove TestApp Projects from sln"
+    Build-WriteTitle "Remove TestApp Projects from sln"
     dotnet sln $solution remove ../src/Simplexcel.MvcTestApp/Simplexcel.MvcTestApp.csproj
     dotnet sln $solution remove ../src/Simplexcel.TestApp/Simplexcel.TestApp.csproj
 
-    Write-Host "dotnet restore"
+    Build-WriteTitle "dotnet restore"
     dotnet restore $solution
 
-    Write-Host "dotnet pack"
+    Build-WriteTitle "dotnet pack"
     # if <Version> is set in the project file, this will not allow the use of a version suffix.
     # => Set <VersionPrefix>1.2.3</VersionPrefix> to only specify the version number.
     # The SDK will then create a Version property based on VersionPrefix and VersionSuffix
     dotnet pack -c $buildConfig -o $artifactsDir -p:PackageVersion=$version$versionSuffix -p:Version=$version$versionSuffix -p:AssemblyVersion=$version -p:FileVersion=$version $solution
 
-    Write-Host "dotnet test"
+    Build-WriteTitle "dotnet test"
     dotnet test "../src/Simplexcel.Tests" -c $buildConfig -r "$artifactsDir/testoutput"
 }
 finally
