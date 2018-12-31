2b5f63bb054b5622b6d0e4357d8c009e25eb5f2f
Michael Stum <104127+mstum@users.noreply.github.com>
2018-12-31 13:45:57 -0500

    Use powershell build (no nuget push yet)

---
+++ scripts/build.ps1
@@ -0,0 +1,61 @@
+﻿param (
+    [switch] $Verbose = $false
+)
+try {
+    if ($Verbose -eq $true)
+    {
+        $PSDefaultParameterValues['*:Verbose'] = $true
+        $VerbosePreference = "Continue"
+        $DebugPreference = "Continue"
+    }
+
+    $InformationPreference = "Continue"
+    $WarningPreference = "Continue"
+    $ErrorActionPreference = "Stop"
+
+    Import-module .\buildFunctions.ps1 -Force
+
+    Build-WriteTitle "Reading Build Properties and Environment"
+    $buildProps = Build-ReadProperties
+    Build-Log-Hashtable $buildProps  
+
+    $solution = Build-GetRequiredProperty $buildProps "build_solution"
+    $version = Build-GetRequiredProperty $buildProps "build_version"
+    $artifactsDir = Build-GetPropertyOrDefault $buildProps "build_artifactsDir" "../artifacts" # Build Server Env
+    $productionBranch = Build-GetPropertyOrDefault $buildProps "build_prod_branch" "master"
+    $buildConfig = Build-GetPropertyOrDefault $buildProps "build_config" "Release"
+
+
+    Build-WriteTitle "Determining Version Number"
+    $commitCount = & git rev-list --count HEAD
+    $branchName = & git rev-parse --abbrev-ref HEAD
+    $part3 = [math]::Floor($commitCount / 65535)
+    $part4 = $commitCount % 65535
+
+    $isProdBuild = $branchName -eq $productionBranch
+    $versionSuffix = $(If ($isProdBuild) { "" } Else {  "-$branchName" })
+
+    $version = "$version.$part3.$part4"
+
+    Write-Host "Version: $version$versionSuffix"
+
+    Write-Host "Remove TestApp Projects from sln"
+    dotnet sln $solution remove ../src/Simplexcel.MvcTestApp/Simplexcel.MvcTestApp.csproj
+    dotnet sln $solution remove ../src/Simplexcel.TestApp/Simplexcel.TestApp.csproj
+
+    Write-Host "dotnet restore"
+    dotnet restore $solution
+
+    Write-Host "dotnet pack"
+    # if <Version> is set in the project file, this will not allow the use of a version suffix.
+    # => Set <VersionPrefix>1.2.3</VersionPrefix> to only specify the version number.
+    # The SDK will then create a Version property based on VersionPrefix and VersionSuffix
+    dotnet pack -c $buildConfig -o $artifactsDir -p:PackageVersion=$version$versionSuffix -p:Version=$version$versionSuffix -p:AssemblyVersion=$version -p:FileVersion=$version $solution
+
+    Write-Host "dotnet test"
+    dotnet test "../src/Simplexcel.Tests" -c $buildConfig -r "$artifactsDir/testoutput"
+}
+finally
+{
+    $PSDefaultParameterValues.Remove('*:Verbose')
+}
---
+++ scripts/buildFunctions.ps1
@@ -0,0 +1,151 @@
+﻿# [CmdletBinding()]
+# -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
+
+function Build-ReadProperties {
+<#
+.SYNOPSIS
+Reads a text file with Key-Value pairs into a Hashtable
+
+.DESCRIPTION
+Expects a text file in the form
+
+key1=value1
+key2=value2
+
+Will return a Hashtable with these associations
+
+.EXAMPLE
+PS> $props = Build-ReadProperties "propsForBuildServer.txt"
+PS> Write-Host "Value of key1: $props.key1"
+
+.LINK
+ConvertFrom-StringData
+
+.PARAMETER propFilename
+The file name to read.
+#>
+    [CmdletBinding()]
+    [OutputType([Hashtable])]
+    param (
+        [Parameter(Position=0,mandatory=$false)]
+        [string]$propFilename="buildProperties.txt",
+
+        [Parameter(Position=1,mandatory=$false)]
+        [switch]$mergeEnvironment=$true
+    )
+
+    Process {
+        $buildProps = Get-Content -Raw $propFilename | ConvertFrom-StringData
+
+        if ($mergeEnvironment) {
+            Get-ChildItem Env: | ForEach-Object -Process { $buildProps[$_.Key] = $_.Value }
+        }
+
+        return $buildProps
+    }
+}
+
+function Build-WriteTitle {
+    param (
+        [AllowEmptyString()]
+        [Parameter(Position=0,mandatory=$true)]
+        [string]$message
+    )
+    Write-Host $message -ForegroundColor Green
+}
+
+function Build-Log-Verbose {
+    param (
+        [AllowEmptyString()]
+        [Parameter(Position=0,mandatory=$true)]
+        [string]$message
+    )
+    Write-Verbose $message
+}
+
+function Build-Log-Information {
+    param (
+        [AllowEmptyString()]
+        [Parameter(Position=0,mandatory=$true,ValueFromPipeline)]
+        [string]$message
+    )
+    Write-Information $message
+}
+
+function Build-Log-Warning {
+    param (
+        [AllowEmptyString()]
+        [Parameter(Position=0,mandatory=$true)]
+        [string]$message
+    )
+    Write-Warning $message
+}
+
+function Build-Log-Error {
+    param (
+        [AllowEmptyString()]
+        [Parameter(Position=0,mandatory=$true)]
+        [string]$message,
+        
+        [AllowEmptyString()]
+        [Parameter(Position=1,mandatory=$false)]
+        [string]$id="BuildError"
+    )
+
+    Write-Error $message -ErrorId $id
+}
+
+function Build-Log-Hashtable {
+    param (
+        [AllowEmptyString()]
+        [Parameter(Position=0,mandatory=$true,ValueFromPipeline)]
+        [Hashtable]$props
+    )
+    $props | Format-Table | Out-String | Build-Log-Information
+}
+
+function Build-GetPropertyOrDefault {
+    param (
+        [Parameter(Position=0,mandatory=$true)]
+        [Hashtable]$props,
+        
+        [AllowEmptyString()]
+        [Parameter(Position=1,mandatory=$true)]
+        [string]$propName,
+        
+        [AllowEmptyString()]
+        [Parameter(Position=2,mandatory=$true)]
+        [string]$defaultValue
+    )
+
+    $value = $props.$propName
+
+    If ([string]::IsNullOrWhiteSpace($value)) {
+        Build-Log-Verbose "$propName was empty, returning '$defaultValue'"
+        return $defaultValue
+    } Else {
+        Build-Log-Verbose "$propName was '$value'"
+        return $value
+    }
+}
+
+function Build-GetRequiredProperty {
+    param (
+        [Parameter(Position=0,mandatory=$true)]
+        [Hashtable]$props,
+        
+        [AllowEmptyString()]
+        [Parameter(Position=1,mandatory=$true)]
+        [string]$propName
+    )
+
+    $value = $props.$propName
+
+    If ([string]::IsNullOrWhiteSpace($value)) {
+        Build-Log-Error "$propName requires a value, but none was given"
+        exit
+
+    } Else {
+        return $value
+    }
+}
